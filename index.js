e2779c05649ada50d593964f8419bb8b43abe000
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-25 09:06:54 -0600

    commit tree 859a617a0029ed373b0960e42fe5472b556bae16

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function fetchBooks() {
+function fetchBooks () {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then(data => renderBooks(data))
+
 }
 
-function renderBooks(books) {
+function renderBooks (books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -12,6 +15,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
